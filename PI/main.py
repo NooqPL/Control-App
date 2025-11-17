b15c09fc6d21fcce1cf806a40209c07f5ecf9a33
Krzysztof Orczyk <149430242+NooqPL@users.noreply.github.com>
2025-11-17 12:46:50 +0100

    Update main.py

--- PI/main.py
+++ PI/main.py
@@ -5,13 +5,16 @@ import time
 def main():
     hw = HardwareController()
 
+    HOST = "0.0.0.0"
     PORT = 5000  # use the same port as windows form
-    server = TCPServer(PORT, hw)
 
-    print(f"[MAIN] Starting TCP server on port {PORT}...")
+    
+    server = TCPServer(HOST, PORT, hw)
+
+    print(f"[MAIN] Starting TCP server on {HOST}:{PORT}...")
 
     try:
-        server.start()
+        server.start_blocking()   
         while True:
             time.sleep(1)
     except KeyboardInterrupt:
@@ -19,8 +22,7 @@ def main():
     except Exception as e:
         print("[MAIN ERROR]", e)
     finally:
-        server.stop()
-
+        pass  
 
 if __name__ == "__main__":
     main()
